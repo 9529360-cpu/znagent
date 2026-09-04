2b5b818bc879a8682a5db90d9d2f2c159d846ccf
9529360-cpu <9529360@gmail.com>
2026-09-04 10:50:24 +0200

    Remove retired maintenance renderer entry

--- apps/desktop/src/zn/main.tsx
+++ apps/desktop/src/zn/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { ZnMaintenanceReportControls } from './maintenance-report-controls'
 import { ZnWorkbench } from './workbench'
 import './styles.css'
 
@@ -13,6 +12,5 @@ if (!root) throw new Error('ZN renderer root is missing')
 createRoot(root).render(
   <StrictMode>
     <ZnWorkbench />
-    <ZnMaintenanceReportControls />
   </StrictMode>
 )
