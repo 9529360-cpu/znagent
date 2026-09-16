38a15c9e17545dccb1e8b0d92b11343d0f045499
9529360-cpu <9529360@gmail.com>
2026-09-16 18:46:21 +0200

    feat: start desktop through provider readiness onboarding

--- apps/desktop/src/zn/main.tsx
+++ apps/desktop/src/zn/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { ZnWorkbench } from './workbench'
+import { ZnProviderOnboarding } from './provider-onboarding'
 import './styles.css'
 
 document.title = 'ZN'
@@ -9,8 +9,9 @@ document.title = 'ZN'
 const root = document.getElementById('root')
 if (!root) throw new Error('ZN renderer root is missing')
 
+// ZnProviderOnboarding hands off to ZnWorkbench after Resident readiness is known.
 createRoot(root).render(
   <StrictMode>
-    <ZnWorkbench />
+    <ZnProviderOnboarding />
   </StrictMode>
 )
